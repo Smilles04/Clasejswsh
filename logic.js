8ddfda865a342bfa234a136df5136f9583728ddb
Williams <williamhenao86@outlook.com>
2023-09-06 21:12:48 -0500

    se agrega juego de palabra

--- logic.js
+++ logic.js
@@ -4,15 +4,21 @@ const lista_tareas = document.getElementById("lista_tareas");
 
 var numAdivinar = generarNum(5);
 
+
+var palabras = ["Manzana", "Carro", "Queso", "Cafe", "Gato"]
+var indicePalabra = generarNum(palabras.length);
+
 function generarNum(numMax){
     return parseInt (Math.random() * numMax);
 }
 
 btn_enviar.addEventListener("click", () => {
-    console.log(numAdivinar);
+    console.log(palabras[indicePalabra]);
+    ///console.log(numAdivinar);
     ///lista_tareas.innerHTML = lista_tareas.innerHTML + "<br>" + sin_name.value;
    ///alert(in_name.value);
-   if(numAdivinar > in_name.value){
+
+  /*  if(numAdivinar > in_name.value){
     alert("El Numero a Adivinar es mayor!, vuelve a intentar");
 
    }else if (numAdivinar < in_name.value){
@@ -20,6 +26,12 @@ btn_enviar.addEventListener("click", () => {
    }else{
     alert("El Numero es correto, Ganaste.!")
     numAdivinar = generarNum(10);
+   } */
+
+   if(palabras[indicePalabra] == in_name.value){
+    alert("Adivinaste la palabra");
+   }else{
+        alert("Sigue Intentando");
    }
     
 } );
